cc5c6f2576af35f31557b17377117655192377a1
KarthickSakthi <karthicksakthi124@gmail.com>
2024-01-15 13:35:39 +0530

    parcel bundler setup added

--- App.js
+++ App.js
@@ -1,3 +1,25 @@
+/**
+ * HMR - Hot Module Reloading
+ * File Watcher Algorithm - c++
+ * Building
+ * Minification
+ * Manages Dev and prod build 
+ * Superfast building Algorithm
+ * Image optimization
+ * Caching while development
+ * Compression
+ * Compatible with older version of browzers
+ * it add polyfills
+ * HTTPS on dev
+ * Manage the port number
+ * Consistent hashing algorithms
+ * Zero config bundler
+ * 
+ * Transitive dependency
+*/
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
 const heading = React.createElement("h1",{id:"heading",xyz:"tag"},"hello world from react");
 const container = React.createElement("div",{id:"parent",xyz:"tag"},
 [React.createElement("div",{id:"child"},
