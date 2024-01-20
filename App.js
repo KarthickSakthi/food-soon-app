90f3461be7dbeb36ca87d5995642a0f5501c1caf
KarthickSakthi <karthicksakthi124@gmail.com>
2024-01-20 17:59:30 +0530

    Component added

--- App.js
+++ App.js
@@ -21,13 +21,14 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const heading = React.createElement("h1",{id:"heading",xyz:"tag"},"hello world from react");
-const container = React.createElement("div",{id:"parent",xyz:"tag"},
-[React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"I am h1 tag"),React.createElement("h1",{},"I am h1 second tag")])] , 
-[React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"I am h1 tag"),React.createElement("h1",{},"I am h1 second tag")])]);
-console.log("heading", heading);
+const Title = ()=>(<div><h1>Namaste</h1></div>) // react Element
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+
+const HeadingComponent = ()=>(
+    <div>
+    {Title()}
+    <h1>Heading Component</h1>
+    </div>
+)
+root.render(<HeadingComponent/>);
 
